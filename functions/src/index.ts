e5d8ee6fbfaa91f836402108a5e1d5bc1ca7aefd
Tommy Wong <tommywong.dev@gmail.com>
2022-12-23 15:17:46 +0800

    structured files and set node engine to 16

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-export * from "./onCreateUser";
+export * from "./triggers/onCreateUser";
--- functions/src/triggers/onCreateUser.ts
+++ functions/src/triggers/onCreateUser.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { User } from "./types";
+import { User } from "../types";
 
 export const onCreateUser = functions.auth.user().onCreate(async (user) => {
   const { displayName, email, photoURL } = user;
